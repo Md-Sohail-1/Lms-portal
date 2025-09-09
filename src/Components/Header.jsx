aa57c4a69bd3c8c88c2dcf0a149ccb453bebdd24
Md Sohail <sohil931376@gmail.com>
2025-09-09 10:57:35 +0530

    minor changes

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -28,7 +28,7 @@ const Header = () => {
             <span className="group-hover:flex group-hover:text-sm hidden md:flex transition duration-150 ease-in-out" >Create Lead</span>
             <IoPersonAddOutline />
           </Link>
-          <button onClick={()=> setDisplayAlert(true)} className="group border-2 border-slate-100 rounded-md flex items-center justify-center gap-2 md:min-h-5 md:min-w-5 p-2 hover:py-1 md:px-4 md:py-1 text-white ease-in-out transition duration-200" >
+          <button onClick={()=> setDisplayAlert(true)} className="group border-2 border-slate-100 rounded-md flex items-center justify-center gap-2 md:min-h-5 md:min-w-5 p-2 hover:py-1 md:px-4 md:py-1 text-white ease-in-out transition duration-200 md:mr-6" >
             <span className="group-hover:flex group-hover:text-sm hidden md:flex transition duration-150 ease-in-out" >Logout</span>
             <CiLogout />
           </button>
